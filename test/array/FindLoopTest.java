88ab8cc46adcbfd2176c0b36419ed77dae23a538
andrey <snab-art@mail.ru>
2020-10-06 21:39:13 +0300

    6.1. Классический поиск перебором. [#378490]

---
+++ test/array/FindLoopTest.java
@@ -0,0 +1,27 @@
+package array;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] input = {5, 4, 3, 2};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas6ThenNoItem() {
+        int[] input = {5, 4, 3, 2};
+        int value = 6;
+        int result = FindLoop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
