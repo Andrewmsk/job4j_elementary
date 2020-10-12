80f2019debe40233d8916f092472e5f6f65dea91
andrey <snab-art@mail.ru>
2020-10-12 19:03:42 +0300

    test

---
+++ src/ru/job4j/Test.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Test {
+    private int max = 10;
+
+    public int min(int[] array) {
+        int max = 0;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.min(new int[]{5, 7}));
+    }
+
+}
