59a3fc838c9fa66b791fb819dacc1d0a7733ea98
andrey <snab-art@mail.ru>
2020-10-13 18:43:15 +0300

    3. Массивы и модели. [#378705]

---
+++ src/ru/job4j/pojo/Library.java
@@ -0,0 +1,23 @@
+package ru.job4j.pojo;
+
+import java.util.Arrays;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book firstBook = new Book("First Book", 500);
+        Book secondBook = new Book("Second Book", 533);
+        Book thirdBook = new Book("Third Book", 545);
+        Book fourthBook = new Book("Clean code", 777);
+
+        Book[] books = {firstBook, secondBook, thirdBook, fourthBook};
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i]);
+        }
+        Book tempBook = books[0];
+        books[0] = books[3];
+        books[3] = tempBook;
+        Arrays.stream(books).forEach(System.out::println);
+        Arrays.stream(books).filter(a -> a.getName().equals("Clean code")).forEach(System.out::println);
+    }
+}
